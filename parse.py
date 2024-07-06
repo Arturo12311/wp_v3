a1877a9618d183bc45629b9133145cd1d135ef06
Arturo12311 <liartur19@gmail.com>
2024-07-05 21:16:43 -0400

    added main function. added more descriptive comments

--- parse.py
+++ parse.py
@@ -1,102 +1,74 @@
 import json
 import re
 
-with open('./data.json', 'r') as file:
-    dataset = json.load(file)
-
 class Chunkify:
     """
-    example usage:
-    chunkify = Chunkify(dataset)
-    list_of_chunks = chunkify.run()
-    
-    outputs a list of dictionaries
-    [{opcode: {chunk_dictionary}}, ...]
+    splits data into useful chunks
     """
-
-    def __init__(self, dataset):
-        self.dataset = dataset
-
-    def run(self):
-        list_of_chunks = self.get_list_of_chunks(self.dataset)
-        return list_of_chunks
+    def __init__(self):
+        pass
 
     def get_list_of_chunks(self, dataset):
-        list_of_chunks = []
-
+        list_of_chunks = [] #output list
         for instance in dataset:
             json_str = instance['ToJsonString']
             opcode = instance['opcode_name']
-
-            chunk = self.chunk_json_string(json_str) # returns dictionary
-            chunk = self.prepend_base_field(chunk)
-            named_chunk = {opcode: chunk}
-            list_of_chunks.append(named_chunk)   
-
-        return list_of_chunks
+            chunked = self.chunk_json_string(json_str) #returns dictionary {fieldname: string/chunk}
+            chunked = self.prepend_base_field(chunked) #makes base field come first
+            formatted = {opcode: chunked}
+            list_of_chunks.append(formatted) #add to output list
+        return list_of_chunks 
     
     def chunk_json_string(self, json_str):
-        chunked = {}
-        
-        split_str = re.split(r'(\\\"[\w.]+\\\")', json_str) # split at fieldnames
-        split_str = split_str[5:]
-        
+        chunked = {} #output dict
+        split_str = re.split(r'(\\\"[\w.]+\\\")', json_str) #split at fieldnames
+        split_str = split_str[5:] #cut unneeded data
         for i in range(0, len(split_str), 2): 
-            key = split_str[i].strip('\\\"') #index 1007 name not found (DAT_090cae0e)
-            value = split_str[i + 1].strip()
+            key = split_str[i].strip('\\\"') #fieldname
+            value = split_str[i + 1].strip() #chunk
             if key != "Members" and key and value:
-                chunked[key] = value
-
-        return chunked
+                chunked[key] = value #add to output dict
+        return chunked 
     
-    def prepend_base_field(self, chunk):
-        if 'Base' in chunk:
-            base_field = {'Base': chunk['Base']}
-            chunk_without_base = {k: v for k, v in chunk.items() if k != 'Base'}
-            base_prepended_chunk = base_field | chunk_without_base
-            return base_prepended_chunk
-        return chunk
+    def prepend_base_field(self, chunked):
+        if 'Base' in chunked:
+            base = {'Base': chunked['Base']} #save base value
+            removed_base = {k: v for k, v in chunked.items() if k != 'Base'}
+            fixed = base | removed_base 
+            return fixed #'base' field comes first now
+        return chunked
 
 class ClassifyChunk:
     """
-    example usage:
-    classify_chunk = ClassifyChunk(chunk)
-    classified = classify_chunk.run()
-
-    takas a chunk as input           {fieldname: string, ...}
-    outputs the classified chunk     {fieldname: type, ...})
+    classifies chunk to specific type
     """
+    def __init__(self) -> None:
+        # regex patterns (struct, enum, map, array, basictype)
+        self.patterns = [
+            '(FTz\\w+)::ToJsonString',
+            'StaticEnum<(ETz\\w+)>',
+            'JsonSerializer<(TMap<[^,]+,TMap<[^,]+,[^,]+)',
+            'JsonSerializer<(TMap<[^,]+,[^,]+)',
+            'JsonSerializer<([^,]*)',
+            'DAT_0901b5c8.*?pcVar10'
+        ]
 
-    def __init__(self, chunk) -> None:
-        self.chunk = chunk 
-        self.classified = {}
+    def classify_chunk(self, chunked):
+        self.classified = {} #output dict
+        for fieldname, chunk in chunked.items(): #for each chunk 
+            type = self.identify_type(chunk) #determine type
+            self.classified[fieldname] = type 
+        return self.classified #return dictionary of classified chunks
 
-    def run(self):
-        for fieldname, string in self.chunk.items(): 
-            type = self.classify_type(string) 
-            self.classified[fieldname] = type
-        return self.classified
-
-    def get_patterns(self):
-        patterns= [
-            "(FTz\w+)::ToJsonString",
-            "StaticEnum<(ETz\w+)>",
-            "JsonSerializer<(TMap<[^,]+,TMap<[^,]+,[^,]+)",
-            "JsonSerializer<(TMap<[^,]+,[^,]+)",
-            "JsonSerializer<([^,]*)"
-            ]
-        return patterns
-
-    def classify_type(self, string):
-        patterns = self.get_patterns()
-        for pattern in patterns:
-            match = re.search(pattern, string)
+    def identify_type(self, chunk):
+        for pattern in self.patterns: #for each pattern 
+            match = re.search(pattern, chunk) #check if pattern in chunk
             if match:
-                type = match.group(1)
-                return type
-        return "unknown"
+                type = match.group(1) 
+                return type #return matched pattern type
+        return "unknown" #chunk didnt match a pattern
 
-def output(opcode, fields_chunk):
+def output_to_console(opcode, fields_chunk): #temporary/testing pusrposes
     print("\n\n---------------------------------------------------")
     print(f'"{opcode}" {{')
     for fieldname, type in fields_chunk.items():
@@ -104,13 +76,24 @@ def output(opcode, fields_chunk):
     print('  }')
 
 
+def main():
+    """
+    1. chunk raw data
+    2. use regex to extract data
+    3. format for output
+    """
 
+    # chunk 
+    with open('./data.json', 'r') as file:
+        dataset = json.load(file)
+    chunkify = Chunkify()
+    list_of_chunks = chunkify.get_list_of_chunks(dataset) 
 
-chunkify = Chunkify(dataset)
-list_of_chunks = chunkify.run() # list of chunks
+    # classify 
+    for chunk in list_of_chunks:
+        [(opcode, chunk)] = chunk.items()    
+        classify = ClassifyChunk()    
+        classified = classify.classify_chunk(chunk)  
+        output_to_console(opcode, classified)
 
-for chunk in list_of_chunks:
-    [(opcode, fields_chunk)] = chunk.items()        
-    classify_chunk = ClassifyChunk(fields_chunk)    
-    fields_chunk = classify_chunk.run()
-    output(opcode, fields_chunk)
+    # output
